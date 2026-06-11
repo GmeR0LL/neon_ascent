d7bb257fcda59ca822264bb454dc8f1fdc9d00ea
Mateusz6587 <mateusz.dul@student.put.poznan.pl>
2026-06-11 23:39:35 +0200

    aktualizacja gracz.h i gracz.cpp

--- NeonAscentv1/Gracz.cpp
+++ NeonAscentv1/Gracz.cpp
@@ -2,16 +2,20 @@
 
 Gracz::Gracz(float x, float y) : Obiekt(x, y) {
     ksztalt.setSize(sf::Vector2f(30.f, 30.f));
-    ksztalt.setFillColor(sf::Color::Cyan); // Nasz neonowy kwadracik
+    ksztalt.setFillColor(sf::Color::Cyan);
     ksztalt.setPosition(pozycja);
 
-    szybkoscRuchu = 400.f; // piksele na sekundę
+    szybkoscRuchu = 400.f;
+    grawitacja = 1200.f;  // Ciągnie w dół (oś Y rośnie w dół w SFML)
+    silaSkoku = -750.f;   // Wybija do góry
 }
 
 void Gracz::aktualizuj(float deltaTime) {
-    predkosc.x = 0.f;
+    // 1. Grawitacja - wektor przyspieszenia
+    predkosc.y += grawitacja * deltaTime;
 
-    // Sterowanie (A/D lub Strzałki)
+    // 2. Sterowanie
+    predkosc.x = 0.f;
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) || sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
         predkosc.x = -szybkoscRuchu;
     }
@@ -19,14 +23,18 @@ void Gracz::aktualizuj(float deltaTime) {
         predkosc.x = szybkoscRuchu;
     }
 
-    // Aplikowanie prędkości poziomej do pozycji (wektory w praktyce)
-    pozycja.x += predkosc.x * deltaTime;
+    // 3. Aktualizacja pozycji o wektor prędkości
+    pozycja += predkosc * deltaTime;
 
-    // Zawijanie ekranu (jak w Icy Tower)
+    // 4. Zawijanie ekranu
     if (pozycja.x < 0) pozycja.x = 800.f;
     if (pozycja.x > 800) pozycja.x = 0.f;
 
     ksztalt.setPosition(pozycja);
+}
+
+void Gracz::skok() {
+    predkosc.y = silaSkoku;
 }
 
 void Gracz::rysuj(sf::RenderWindow& okno) {
--- NeonAscentv1/Gracz.h
+++ NeonAscentv1/Gracz.h
@@ -7,12 +7,16 @@ class Gracz : public Obiekt {
 private:
     sf::RectangleShape ksztalt;
     float szybkoscRuchu;
+    float grawitacja;
+    float silaSkoku;
 
 public:
     Gracz(float x, float y);
     void aktualizuj(float deltaTime) override;
     void rysuj(sf::RenderWindow& okno) override;
     sf::FloatRect pobierzGranice() const override;
+    void skok();
+    float pobierzPredkoscY() const { return predkosc.y; }
 };
 
 #endif // GRACZ_H
