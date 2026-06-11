d6e6ab0bc495bbc703651314eef66244769ede55
GmeR0LL <wiktor.gmerek@student.put.poznan.pl>
2026-06-11 21:55:44 +0200

    Utworzenie podstawy gry, żeby się uruchamiała

--- NeonAscentv1/main.cpp
+++ NeonAscentv1/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include "gra.h"
+#include "platforma.h"
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
+int main(){
+    gra.run();
     return 0;
 }
