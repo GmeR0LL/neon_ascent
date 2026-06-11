243d8edff6ca0d54189e68fd5401cde5b064097a
Mateusz6587 <mateusz.dul@student.put.poznan.pl>
2026-06-11 23:30:58 +0200

    stworzenie gracz.h

--- NeonAscentv1/gracz.h
+++ NeonAscentv1/gracz.h
@@ -1,4 +1,18 @@
 #ifndef GRACZ_H
 #define GRACZ_H
 
+#include "Obiekt.h"
+
+class Gracz : public Obiekt {
+private:
+    sf::RectangleShape ksztalt;
+    float szybkoscRuchu;
+
+public:
+    Gracz(float x, float y);
+    void aktualizuj(float deltaTime) override;
+    void rysuj(sf::RenderWindow& okno) override;
+    sf::FloatRect pobierzGranice() const override;
+};
+
 #endif // GRACZ_H
