e03b766542ab44e8a6bf892ab919dbe0bc94a924
GmeR0LL <wiktor.gmerek@student.put.poznan.pl>
2026-06-11 21:09:24 +0200

    Konfiguracja cmakelist
    Konfiguracja cmakelist.txt według instrukcji

--- NeonAscentv1/build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/cmake_install.cmake
+++ NeonAscentv1/build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/cmake_install.cmake
@@ -37,16 +37,6 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "C:/Qt/Tools/mingw1310_64/bin/objdump.exe")
 endif()
 
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "C:/Users/LenOvO/Desktop/github/neon_ascent/NeonAscentv1/build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/NeonAscentv1.exe")
-  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/NeonAscentv1.exe" AND
-     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/NeonAscentv1.exe")
-    if(CMAKE_INSTALL_DO_STRIP)
-      execute_process(COMMAND "C:/Qt/Tools/mingw1310_64/bin/strip.exe" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/NeonAscentv1.exe")
-    endif()
-  endif()
-endif()
-
 if(CMAKE_INSTALL_COMPONENT)
   if(CMAKE_INSTALL_COMPONENT MATCHES "^[a-zA-Z0-9_.+-]+$")
     set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
