7189473c71d6f1a47930a99adacc997864109b7f
Mateusz6587 <mateusz.dul@student.put.poznan.pl>
2026-06-12 10:24:29 +0200

    zmiany w platforma.cpp

--- NeonAscentv1/Platforma.cpp
+++ NeonAscentv1/Platforma.cpp
@@ -2,9 +2,22 @@
 
 
 //platforma dodalem boola
-Platforma::Platforma(float x, float y, float szerokosc) : Obiekt(x, y), czyDotknieta(false) {
+Platforma::Platforma(float x, float y, float szerokosc, TypPlatformy typPlatformy) : Obiekt(x, y), czyDotknieta(false), typ(typPlatformy), zegarMigotania(0.f), widoczna(true), zniszczona(false) {
     ksztalt.setSize(sf::Vector2f(szerokosc, 20.f));
-    ksztalt.setFillColor(sf::Color(255, 0, 255)); // Jasny neonowy róż
+    // Kolory platform
+    if(typ == TypPlatformy::ZWYKLA)
+    {
+        ksztalt.setFillColor(sf::Color(255,0,255)); // Różowa
+    }
+    else if(typ == TypPlatformy::KRUCHA)
+    {
+        ksztalt.setFillColor(sf::Color(255, 100, 0)); //Pomaranczowa(zanika)
+    }
+    else if (typ == TypPlatformy::MIGAJACA)
+    {
+        ksztalt.setFillColor(sf::Color(0, 255, 255)); // Błękitna (miga)
+    }
+
     ksztalt.setPosition(pozycja);
 }
 
@@ -12,11 +25,21 @@ Platforma::Platforma(float x, float y, float szerokosc) : Obiekt(x, y), czyDotkn
 
 
 void Platforma::aktualizuj(float deltaTime) {
-
+    if (typ == TypPlatformy::MIGAJACA && !zniszczona) {
+        //Odmierza czas dla platform migajacych
+        zegarMigotania += deltaTime;
+        if (zegarMigotania >= 2.0f) { // Co 2 sekundy
+            widoczna = !widoczna;     // zmiana widocznosci
+            zegarMigotania = 0.f;
+        }
+    }
 }
 
 void Platforma::rysuj(sf::RenderWindow& okno) {
-    okno.draw(ksztalt);
+    //Rysujemy, gdy jest widoczna i nie jest zniszczona
+    if (widoczna && !zniszczona) {
+        okno.draw(ksztalt);
+    }
 }
 
 sf::FloatRect Platforma::pobierzGranice() const {
@@ -27,5 +50,12 @@ sf::FloatRect Platforma::pobierzGranice() const {
 //dotknieta platforma
 void Platforma::oznaczJakoDotknieta() {
     czyDotknieta = true;
+
+    if (typ == TypPlatformy::KRUCHA) {
+        zniszczona = true;
+    }
+    else if (typ == TypPlatformy::ZWYKLA) {
+        ksztalt.setFillColor(sf::Color(100, 0, 100)); // Zwykła tylko ciemnieje
+    }
     ksztalt.setFillColor(sf::Color(100, 0, 100)); //ciemny roz
-}
+ }
